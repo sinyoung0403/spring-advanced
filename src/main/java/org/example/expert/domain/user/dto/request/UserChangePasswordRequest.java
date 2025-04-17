3b7f3c44d98ba616dc6c971b94747417395eab5e
sinyoung0403 <tlstjs753@gmail.com>
2025-04-17 11:25:19 +0900

    Lv1 -3: Validation
    1. @Pattern : 정규 표현식 추가
    2. @Size : 비밀번호 최소 길이
    3. Controller 에 @Valid 추가
    4. build 에 validated 추가

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,8 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +14,9 @@ public class UserChangePasswordRequest {
 
     @NotBlank
     private String oldPassword;
+
+    @Size(min = 8, message = "비밀번호는 최소 8자 이상이어야 합니다.")
+    @Pattern(regexp = "^(?=.*\\d)(?=.*[A-Z]).*$", message = "숫자와 대문자를 포함해야 합니다.")
     @NotBlank
     private String newPassword;
 }
